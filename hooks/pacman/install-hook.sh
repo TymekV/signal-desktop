d6b7605adf138980f7e4b41685a19ec5225ceacf
Tymek <vonautymek@gmail.com>
2026-05-10 17:41:59 +0200

    fixed various colors

--- hooks/pacman/install-hook.sh
+++ hooks/pacman/install-hook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p /usr/local/lib/pacman
 cp patch-signal-desktop.sh /usr/local/lib/pacman/patch-signal-desktop.sh
+cp -r ../../themes/* /usr/local/lib/pacman/signal-themes/
 cp 10-catppuccin-signal.hook /etc/pacman.d/hooks/10-catppuccin-signal.hook
--- hooks/pacman/patch-signal-desktop.sh
+++ hooks/pacman/patch-signal-desktop.sh
@@ -3,6 +3,6 @@ FLAVOR=mocha
 TEMP=$(mktemp -d)
 SIGNAL_DIR="/usr/lib/signal-desktop/resources"
 asar e "${SIGNAL_DIR}/app.asar" ${TEMP}
-curl "https://raw.githubusercontent.com/CalfMoon/signal-desktop/refs/heads/main/themes/catppuccin-${FLAVOR}.css" -o "${TEMP}/stylesheets/catppuccin-${FLAVOR}.css"
+cp "/usr/local/lib/pacman/signal-themes/catppuccin-${FLAVOR}.css" "${TEMP}/stylesheets/catppuccin-${FLAVOR}.css"
 sed -i "1i @import \"catppuccin-${FLAVOR}.css\";" "${TEMP}/stylesheets/manifest.css"
 sudo asar p ${TEMP} "${SIGNAL_DIR}/app.asar"
